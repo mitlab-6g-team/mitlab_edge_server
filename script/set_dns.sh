0e82e2e378b70823e44f029ec64126950f522333
hpc0326 <jack20010236@gmail.com>
2025-01-02 22:00:25 -0800

    fix: arg problem

--- script/set_dns.sh
+++ script/set_dns.sh
@@ -6,9 +6,9 @@ source .env.common
 # set docker insecure list
 # ==================================
 
-sudo bash -c " cat <<EOR > "/etc/docker/daemon.json"
+sudo bash -c "cat <<EOR > /etc/docker/daemon.json
 {
-    "insecure-registries" : ["$DEPLOYMENT_PF_HOST_IP", "$DEPLOYMENT_PF_HOST_IP:80"]
+    \"insecure-registries\": [\"$DEPLOYMENT_PF_HOST_IP\", \"$DEPLOYMENT_PF_HOST_IP:80\"]
 }
 EOR"
 
@@ -16,7 +16,7 @@ EOR"
 # edit docker.service
 # ==================================
 sudo sed -i '/^ExecStart=/s/^/# /' /lib/systemd/system/docker.service
-sudo sed -i '/^# ExecStart=/a\ExecStart = \nExecStart =/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:35432' /lib/systemd/system/docker.service
+sudo sed -i '/^# ExecStart=/aExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:35432' /lib/systemd/system/docker.service
 
 # ==================================
 # set containerd configuration
@@ -25,21 +25,19 @@ sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
 
 # # mirrors
-sudo sed -i "/.registry.mirrors/a\\
-[plugins."io.containerd.grpc.v1.cri".registry.mirrors."$DEPLOYMENT_PF_HOST_IP"]\nendpoint = ["http://$DEPLOYMENT_PF_HOST_IP"]" "/etc/containerd/config.toml"
+sudo sed -i "/\[plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors\]/a\\
+[plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"$DEPLOYMENT_PF_HOST_IP\"]\n  endpoint = [\"http://$DEPLOYMENT_PF_HOST_IP\"]" /etc/containerd/config.toml
 
 # # configs
-sudo sed -i "/.registry.configs/a\\
-[plugins."io.containerd.grpc.v1.cri".registry.configs."$DEPLOYMENT_PF_HOST_IP".tls]\n insecure_skip_verify = true" "/etc/containerd/config.toml"
-
+sudo sed -i "/\[plugins.\"io.containerd.grpc.v1.cri\".registry.configs\]/a\\
+[plugins.\"io.containerd.grpc.v1.cri\".registry.configs.\"$DEPLOYMENT_PF_HOST_IP\".tls]\n  insecure_skip_verify = true" /etc/containerd/config.toml
 
 # ==================================
 # edit containerd
 # ==================================
 sudo mkdir -p /etc/systemd/system/containerd.service.d
 
-
-sudo bash -c "cat <<EOF >/etc/systemd/system/containerd.service.d/override.conf
+sudo bash -c "cat <<EOF > /etc/systemd/system/containerd.service.d/override.conf
 [Service]
 ExecStart=
 ExecStart=/usr/bin/containerd --config /etc/containerd/config.toml
